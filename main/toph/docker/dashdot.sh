239d5e30c5bc8c651db6e4cab80b6f26830e9230
Adam Langbert <adamhl@pm.me>
2022-06-04 21:46:08 +0200

    update dashdot config

--- main/toph/docker/dashdot.sh
+++ main/toph/docker/dashdot.sh
@@ -13,9 +13,9 @@ services:
     privileged: true
     network_mode: host
     environment:
+      - DASHDOT_WIDGET_LIST=os,cpu,ram,network
       - DASHDOT_PORT=8005
       - DASHDOT_ENABLE_CPU_TEMPS=true
-      - DASHDOT_STORAGE_WIDGET_ENABLE=false
       - DASHDOT_NETWORK_WIDGET_MIN_WIDTH=1200
       - DASHDOT_OVERRIDE_OS=Debian sid
 EOF
