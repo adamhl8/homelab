661cdacf52909d004f113f67a4a2cd7060e38cc8
Adam Langbert <adamhl@pm.me>
2022-09-15 12:26:11 -0500

    type

--- common/sops.sh
+++ common/sops.sh
@@ -3,7 +3,7 @@
 curl -s https://api.github.com/repos/mozilla/sops/releases/latest | grep -o -E "https://(.*)sops(.*)linux.amd64" | sed 1q | xargs curl -Lo ~/bin/sops
 chmod 755 ~/bin/sops
 
-mkdr -p ~/.config/sops/age/
+mkdir -p ~/.config/sops/age/
 echo "Copy age keys file to ~/.config/sops/age/"
 continue_prompt
 
