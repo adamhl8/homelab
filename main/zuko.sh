cad49aeaee1e90adacbbe040837219b220cd4418
Adam Langbert <adamhl@pm.me>
2022-09-18 16:32:47 -0500

    zuko

--- main/zuko.sh
+++ main/zuko.sh
@@ -4,22 +4,28 @@ steps=2
 
 step1() {
   echo 'deb http://deb.debian.org/debian/ unstable main' | sudo tee /etc/apt/sources.list
-  source ${common}/common.sh
-  source ${common}/ssh.sh
+  ~/homelab/common/common.sh
+  ~/homelab/common/ssh.sh
 
   sudo sed -i "s|127\.0\.1\.1.*|127.0.1.1       zuko|" /etc/hosts
-  mkdir ~/apps/
 
   # Argon fan script
   curl https://download.argon40.com/argon1.sh | bash
   
-  source ${common}/docker.sh
+  ~/homelab/common/docker.sh
 }
 
 step2() {
-  source ${modules}/adguard_home.sh
-  source ${modules}/uptime_kuma.sh
+  ln -s ${modules}/docker/ ~/
+  ln -s ~/homelab/common/docker/tailscale/ ~/docker/
 
-  source ${common}/tailscale.sh
-  docker exec tailscale tailscale up
+  for d in ~/docker/*/; do
+    cd ${d}
+    ${d}/init.sh
+    sdc
+    ${d}/fini.sh
+  done
+  cd ~/
+
+  docker exec tailscale tailscale up --accept-dns=false
 }
