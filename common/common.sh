51b3d514cf076a7a8d8455243b6a8f7482b5a7db
Adam Langbert <adamhl@pm.me>
2022-09-19 13:13:41 -0500

    git ssh

--- common/common.sh
+++ common/common.sh
@@ -14,6 +14,10 @@ git config --global user.name 'Adam Langbert'
 git config --global user.email 'adamhl@pm.me'
 git config --global pull.ff only
 
+cd ~/homelab/
+git remote set-url origin git@github.com:$(git remote get-url origin | sed 's|https://github.com/||')
+cd ~/
+
 sudo -v
 cd /usr/bin/
 curl https://getmic.ro/r | sudo sh
