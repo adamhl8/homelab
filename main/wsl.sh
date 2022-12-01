3144006807176e72c32e76d4a4962d9faaf47144
Adam Langbert <adamhl@pm.me>
2022-12-01 00:30:10 -0600

    Update wsl.sh

--- main/wsl.sh
+++ main/wsl.sh
@@ -17,19 +17,6 @@ step2() {
 
   mkdir ~/dev/
 
-  curl -Lo ~/awscli.zip https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip
-  unzip ~/awscli.zip
-  rm ~/awscli.zip
-  sudo ~/aws/install
-  rm -rf ~/aws/
-
-  echo "URL: https://nw-computing.awsapps.com/start#/"
-  echo "Region: us-west-2"
-  aws configure sso --profile nw-computing
-  echo "export AWS_SDK_LOAD_CONFIG=1" | tee -a ~/.bashrc
-  echo "export AWS_PROFILE=nw-computing" | tee -a ~/.bashrc
-  echo "export DATADOG_API_KEY=" | tee -a ~/.bashrc
-
   curl -s "https://get.sdkman.io" | bash
   source ~/.sdkman/bin/sdkman-init.sh
   sdk install java 18.0.2.1-open
