4fcd42e45003755ba2ecc4bd816269ac38caf656
Adam Langbert <adamhl@pm.me>
2023-02-26 15:42:08 -0600

    adguard

--- utils/helpers.py
+++ utils/helpers.py
@@ -10,12 +10,18 @@ def get_arch():
 def add_user(user: str='adam'):
   user_home=f'/home/{user}'
 
+  X('apt install sudo')
+
   X(f'adduser {user}')
   X(f'usermod -aG sudo {user}')
-  X(f'cp -a {HOMELAB_ROOT}/ {user_home}/')
+
   X(f'mkdir -p {user_home}/.ssh/')
   X(f'chmod 700 {user_home}/.ssh/')
   X(f'chown {user}:{user} {user_home}/.ssh/')
   X(f'cp -f {COMMON}/authorized_keys {user_home}/.ssh/')
   X(f'chmod 600 {user_home}/.ssh/authorized_keys')
   X(f'chown {user}:{user} {user_home}/.ssh/authorized_keys')
+
+  X(f'cp -a {HOMELAB_ROOT}/ {user_home}/')
+  X(f'chown -R {user}:{user} {HOMELAB_ROOT}/')
+  X(f'touch {HOMELAB_ROOT}/step1')
