7c479159c08959a1117d128d7c835904fe3522a1
Adam Langbert <adamhl@pm.me>
2023-02-26 15:29:25 -0600

    add sudo fallback to init

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install python3 python3-pip python-is-python3 -y
+if type sudo &>/dev/null; then
+  sudo apt update
+  sudo apt install python3 python3-pip python-is-python3 -y
+else
+  apt update
+  apt install python3 python3-pip python-is-python3 -y
+fi
