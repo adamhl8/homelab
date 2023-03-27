279cf3c4f6dcc982d69aecbc6941108db4664683
Adam Langbert <adamhl@pm.me>
2023-03-26 22:33:37 -0500

    caddy

--- nodes/_sid/docker/caddy/init.py
+++ nodes/_sid/docker/caddy/init.py
@@ -1,5 +1,7 @@
 import hl_helpers as helpers
 from shellrunner import X
 
-helpers.generate_docker_env(["aws_secret_access_key", "caddy_zerossl_key"], __file__)
-X("sops exec-env ~/secrets.yaml 'envsubst < ~/docker/caddy/users.json | tee ~/docker/caddy/users.json > /dev/null'")
+helpers.generate_docker_env(["aws_secret_access_key"], __file__)
+X(
+    r"""cat ~/docker/caddy/users.json | string replace '${caddy_security_2fa_secret}' (sops -d --extract "['caddy_security_2fa_secret']" ~/secrets.yaml) | tee ~/docker/caddy/users.json > /dev/null""",
+)
