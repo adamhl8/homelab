cb5c6ed97eab3bbb1187358856407ef3cb81d3e7
Adam Langbert <adamhl@pm.me>
2023-03-28 22:13:53 -0500

    storage

--- nodes/_sid/storage/init.py
+++ nodes/_sid/storage/init.py
@@ -1,20 +1,20 @@
 def main():
+    from hl_helpers import homelab_paths as paths
     from shellrunner import X
 
     X("sudo apt install btrfs-progs -y")
+    X("sudo apt install fuse -y")
+    X("~/bin/mergerfs-update.py")
 
-    # data disks
-    X("sudo mkdir /mnt/disk{1,2,3}")
-    X('echo "LABEL=disk1 /mnt/disk1 btrfs subvol=/data 0 0" | sudo tee -a /etc/fstab')
-    X('echo "LABEL=disk2 /mnt/disk2 btrfs subvol=/data 0 0" | sudo tee -a /etc/fstab')
-    X('echo "LABEL=disk3 /mnt/disk3 btrfs subvol=/data 0 0" | sudo tee -a /etc/fstab')
+    X("sudo mkdir /mnt/disk{1,2,3}/")
+    X("sudo mkdir /mnt/parity1/")
+    X("sudo mkdir /mnt/storage/")
 
-    # parity disk
-    X("sudo mkdir /mnt/parity1")
-    X('echo "LABEL=parity1 /mnt/parity1 ext4 defaults 0 0" | sudo tee -a /etc/fstab')
+    X("sudo mkdir /etc/fstab.d/")
+    X(f"sudo ln -s {paths.nodes.sid}/storage/mergerfs.fstab /etc/fstab.d/")
 
     X("sudo systemctl daemon-reload")
-    X("sudo mount -a")
+    X("sudo mount --fstab /etc/fstab.d/mergerfs.fstab -a")
 
     # cleanup
     X("sudo btrfs subvolume delete /mnt/disk*/.snapshots/*/snapshot")
@@ -22,13 +22,3 @@ def main():
     X("sudo chown -R adam:adam /mnt/disk*")
     X(r"find /mnt/disk* -type d -exec sudo chmod 755 {} \;")
     X(r"find /mnt/disk* -type f -exec sudo chmod 644 {} \;")
-
-    # mergerfs
-    X("sudo apt install fuse -y")
-    X("~/bin/mergerfs-update.py")
-    X("sudo mkdir /mnt/storage")
-    X(
-        'echo "/mnt/disk* /mnt/storage fuse.mergerfs allow_other,use_ino,cache.files=off,dropcacheonclose=true,category.create=mfs,fsname=mergerfs 0 0" | sudo tee -a /etc/fstab',
-    )
-    X("sudo systemctl daemon-reload")
-    X("sudo mount -a")
