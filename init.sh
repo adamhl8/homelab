3c2c7d1dd93312f25bcabad41d78ffcbf080fc64
Adam Langbert <adamhl@pm.me>
2023-04-02 03:47:01 -0500

    init

--- init.sh
+++ init.sh
@@ -13,16 +13,14 @@ shared="${init}/shared"
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install curl -y
-sudo apt install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+sudo apt install build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 curl https://pyenv.run | bash
-export PYENV_ROOT="~/.pyenv"
+export PYENV_ROOT="$HOME/.pyenv"
 command -v pyenv > /dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
 pyenv install 3.10
 pyenv global 3.10
-if [ "${HOSTNAME}" = "sid" ]; then pip install --break-system-packages -U python-shellrunner
-else pip install -U python-shellrunner
-fi
+pip install -U python-shellrunner
 
 pth_file="$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")/homelab_lib.pth"
 echo "${homelab_root}/lib/" | sudo tee "${pth_file}" > /dev/null
