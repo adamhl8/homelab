d58979b794ad74a7be9382736390095183160c41
Adam Langbert <adamhl@pm.me>
2023-04-16 00:59:16 -0500

    remove zigbee

--- nodes/_sid/docker/zigbee2mqtt/fini.py
+++
@@ -1,9 +0,0 @@
-from time import sleep
-
-from shellrunner import X
-
-print("Waiting for Zigbee2MQTT to start...")
-sleep(5)
-
-X("sudo ln -s -f ~/docker/zigbee2mqtt/zigbee2mqtt_config.yaml ~/docker/zigbee2mqtt/data/zigbee2mqtt/configuration.yaml")
-X("docker restart zigbee2mqtt")
