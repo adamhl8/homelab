dc71ffa688cc4603b3038eeb5f83082d4c4361bd
Adam Langbert <adamhl@pm.me>
2023-04-23 20:34:58 -0500

    pve

--- nodes/pve.py
+++ nodes/pve.py
@@ -4,12 +4,12 @@ from shellrunner import X
 
 def step1():
     X("apt install nginx")
-    X("rm /etc/nginx/sites-enabled/default")
+    X("rm -f /etc/nginx/sites-enabled/default")
     X(f"ln -f -s {paths.nodes.pve}/proxmox.conf /etc/nginx/conf.d/proxmox.conf")
     X("nginx -t")
     X("systemctl restart nginx")
 
-    X("mkdir /etc/systemd/system/nginx.service.d/")
+    X("mkdir -p /etc/systemd/system/nginx.service.d/")
     X(f"ln -f -s {paths.nodes.pve}/override.conf /etc/systemd/system/nginx.service.d/")
 
     X("apt install smartmontools -y")
