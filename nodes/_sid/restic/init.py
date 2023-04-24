26a656f5baa160933339b48c8763c722f2742449
Adam Langbert <adamhl@pm.me>
2023-04-23 20:28:08 -0500

    restic init

--- nodes/_sid/restic/init.py
+++ nodes/_sid/restic/init.py
@@ -8,7 +8,7 @@ def main():
     X(
         'curl -s https://api.github.com/repos/restic/restic/releases/latest | string match -r "https://.*/download/.*restic.*linux_amd64.bz2" | sed 1q | xargs curl -Lo ~/restic/restic.bz2',
     )
-    X("bzip2 -d ~/restic/restic.bz2")
+    X("bzip2 -df ~/restic/restic.bz2")
     X("chmod 755 ~/restic/restic")
 
 
