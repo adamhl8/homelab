d9d7a25d53bec907c3905d99e73d5ccd2b60d76b
Adam Langbert <adamhl@pm.me>
2023-05-31 21:09:06 -0500

    system-update

--- init/shared/shellrunner_install.bash
+++ init/shared/shellrunner_install.bash
@@ -4,6 +4,6 @@ brew install python@3.11
 pip install --upgrade pip
 pip install -U python-shellrunner
 
-pth_file="$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")/homelab_lib.pth"
+pth_file="$(python3 -c "import sysconfig; print(sysconfig.get_path('purelib'))")/homelab_lib.pth"
 echo "${homelab_root}/lib/" | sudo tee "${pth_file}" >/dev/null
 echo "Added homelab/lib/ to python search path."
