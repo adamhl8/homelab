56a046f61053a228b393750f8fccf45fc950ad6b
Adam Langbert <adamhl@pm.me>
2023-08-06 11:33:00 -0500

    cleanup.fish

--- nodes/_macbook/bin/cleanup.fish
+++ nodes/_macbook/bin/cleanup.fish
@@ -1,13 +1,16 @@
 #!/usr/bin/env fish
 
+rm -rf ~/.android
 rm -rf ~/.cache
 rm -rf ~/.cups
 rm -rf ~/.gradle
 rm -rf ~/.hawtjni
 rm -rf ~/.lesshst
 rm -rf ~/.matplotlib
+rm -rf ~/.m2
 rm -rf ~/.node_repl_history
 rm -rf ~/.npm
+rm -rf ~/.pnpm-state
 rm -rf ~/.python_history
 rm -rf ~/.sonarlint
 rm -rf ~/.sts4
@@ -15,6 +18,8 @@ rm -rf ~/.yarn
 rm -rf ~/.yarnrc
 rm -rf ~/Movies
 rm -rf ~/Music
+rm -rf ~/.bash_history
+rm -rf ~/.zsh_history
 
 fd -ui -tf '^\.DS_Store' ~ -x rm -f
 fd -ui -tf '^\.localized' ~ -x rm -f
