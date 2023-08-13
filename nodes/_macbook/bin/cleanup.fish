45bdaa0ddb4fe8b6c8958a6a66477f4a96d5ed13
Adam Langbert <adamhl@pm.me>
2023-08-13 14:03:08 -0500

    cleanup.fish

--- nodes/_macbook/bin/cleanup.fish
+++ nodes/_macbook/bin/cleanup.fish
@@ -21,6 +21,26 @@ rm -rf ~/Music
 rm -rf ~/.bash_history
 rm -rf ~/.zsh_history
 
-fd -ui -tf '^\.DS_Store' ~ -x rm -f
-fd -ui -tf '^\.localized' ~ -x rm -f
-fd -ui -tf '^\._' ~ -x rm -f
+set ds_store (fd -ui -tf '^\.DS_Store$' ~)
+if test -n "$ds_store"
+  printf '%s\n' $ds_store
+  read -n 1 -P "Delete? [Y/n] " reply
+  test $reply != n -a $reply != N
+  and rm -f $ds_store
+end
+
+set localized (fd -ui -tf '^\.localized$' ~)
+if test -n "$localized"
+  printf '%s\n' $localized
+  read -n 1 -P "Delete? [Y/n] " reply
+  test $reply != n -a $reply != N
+  and rm -f $localized
+end
+
+set dotu (fd -ui -tf '^\._' ~)
+if test -n "$dotu"
+  printf '%s\n' $dotu
+  read -n 1 -P "Delete? [Y/n] " reply
+  test $reply != n -a $reply != N
+  and rm -f $dotu
+end
