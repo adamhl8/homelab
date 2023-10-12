a5192d4ef7f899a3bc62aa2ed6eee7114a60f8cd
Adam Langbert <adamhl@pm.me>
2023-10-12 10:16:34 -0700

    tide

--- shared/fish_setup.py
+++ shared/fish_setup.py
@@ -4,11 +4,12 @@ def main():
     X(
         "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher",
     )
-    X("fisher install IlanCosman/tide@v5")
-    X("echo 2 1 2 3 1 1 1 1 1 1 y | tide configure >/dev/null")
+    X("fisher install IlanCosman/tide")
+    X("echo 2 1 2 3 1 1 1 1 1 1 1 y | tide configure >/dev/null")
+    X("set -l ind (contains -i -- kubectl $tide_right_prompt_items); and set -e tide_right_prompt_items[$ind]")
+
 
     X("rye self completion -s fish >~/.config/fish/completions/rye.fish")
-
 
 if __name__ == "__main__":
     main()
