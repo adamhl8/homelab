b14e22cc372d70e3622b9b32cf9ee0ab92f5c32b
Adam Langbert <adamhl@pm.me>
2023-10-18 14:49:55 -0700

    fix snapraid_update

--- nodes/_sid/bin/snapraid_update.py
+++ nodes/_sid/bin/snapraid_update.py
@@ -4,12 +4,12 @@ from shellrunner import X
 
 
 def main():
-    X("mkdir ~/`tmp/")
+    X("mkdir ~/tmp/")
     X(["cd ~/tmp/", "git clone https://github.com/ironicbadger/docker-snapraid.git"])
     X(["cd ~/tmp/docker-snapraid/", "~/tmp/docker-snapraid/build.sh"], show_output=False)
     X("dpkg-deb -x ~/tmp/docker-snapraid/build/snapraid-from-source.deb ~/tmp/")
     X("mv -f ~/tmp/usr/local/bin/snapraid ~/bin/")
-    X("rm -rf ~`/tmp/")
+    X("rm -rf ~/tmp/")
 
 
 if __name__ == "__main__":
