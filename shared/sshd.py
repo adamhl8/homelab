457712d4923d057c761bcbba3c3d3d3838cb0fae
Adam Langbert <adamhl@pm.me>
2023-10-18 16:03:14 -0700

    sshd

--- shared/sshd.py
+++ shared/sshd.py
@@ -2,7 +2,14 @@ def main():
     from hl_helpers import homelab_paths as paths
     from shellrunner import X
 
+    X("sudo rm -f /etc/ssh/ssh_host_*")
+    X('ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""')
+    X('ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""')
+
+    X("awk '$5 >= 3071' /etc/ssh/moduli | sudo tee /etc/ssh/moduli.safe")
+
     X(f"sudo ln -f -s {paths.configs.sshd_config} /etc/ssh/")
+    X("sudo sshd -T >/dev/null")
     X("sudo systemctl restart ssh")
 
 
