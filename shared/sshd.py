99ab71493be49bd01d5404b12e251c70ce6c5776
Adam Langbert <adamhl@pm.me>
2023-10-18 16:23:29 -0700

    sshd

--- shared/sshd.py
+++ shared/sshd.py
@@ -6,7 +6,7 @@ def main():
     X('sudo ssh-keygen -t rsa -b 4096 -f /etc/ssh/ssh_host_rsa_key -N ""')
     X('sudo ssh-keygen -t ed25519 -f /etc/ssh/ssh_host_ed25519_key -N ""')
 
-    X("awk '$5 >= 3071' /etc/ssh/moduli | sudo tee /etc/ssh/moduli.safe")
+    X("awk '$5 >= 4095' /etc/ssh/moduli | sudo tee /etc/ssh/moduli.safe >/dev/null")
 
     X(f"sudo ln -f -s {paths.configs.sshd_config} /etc/ssh/")
     X("sudo sshd -T >/dev/null")
