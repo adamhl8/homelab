8297c92e38b21eacbe8e4ef9488afaeed2798aa9
Adam Langbert <adamhl@pm.me>
2023-10-29 16:48:35 -0500

    remove vaultwarden

--- nodes/_sid/docker/vaultwarden/init.py
+++
@@ -1,9 +0,0 @@
-from hl_helpers import generate_docker_env
-
-
-def main() -> None:
-    generate_docker_env(["vaultwarden_admin_token", "aws_access_key_id", "smtp_password"], __file__)
-
-
-if __name__ == "__main__":
-    main()
