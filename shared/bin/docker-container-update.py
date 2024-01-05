a9de61346e6ede0afe7ed7d1ee72e0b29a16581c
Adam Langbert <adamhl@pm.me>
2024-01-05 15:51:21 -0600

    docker-container-update

--- shared/bin/docker-container-update.py
+++ shared/bin/docker-container-update.py
@@ -4,6 +4,7 @@
 
 import os
 from collections import defaultdict
+import json
 
 from pathlib import Path
 from typing import NamedTuple
@@ -18,8 +19,9 @@ os.environ["SHELLRUNNER_SHOW_COMMAND"] = "False"
 class ContainerDetails(NamedTuple):
     image_name: str
     service_name: str
-    is_up_to_date: bool
     compose_file_path: Path
+    build: bool
+    is_up_to_date: bool
 
 
 def get_running_containers() -> list[tuple[str, str]]:
@@ -31,11 +33,6 @@ def get_running_containers() -> list[tuple[str, str]]:
 
 def get_container_details(container_id: str, image: str) -> ContainerDetails | None:
     try:
-        image_id = X(f"docker inspect {container_id}" + " --format '{{.Image}}'").out
-        image_digest = X(f"docker image inspect {image_id}" + " --format '{{.RepoDigests}}'").out
-        remote_digest = X(f"regctl image digest {image}").out
-        is_up_to_date = remote_digest in image_digest
-
         image_name = X(f"docker inspect {container_id}" + """ --format='{{.Config.Image}}'""").out
         service_name = X(
             f"docker inspect {container_id}" + """ --format '{{ index .Config.Labels "com.docker.compose.service" }}'"""
@@ -46,7 +43,18 @@ def get_container_details(container_id: str, image: str) -> ContainerDetails | N
         ).out
         compose_file_path = Path(compose_file).resolve(strict=True)
 
-        return ContainerDetails(image_name, service_name, is_up_to_date, compose_file_path)
+        compose_config = X(f"docker compose -f {compose_file_path} config --format json").out
+        compose_config = json.loads(compose_config)
+        build = "build" in compose_config["services"][service_name]
+
+        is_up_to_date = False
+        if not build:
+            image_id = X(f"docker inspect {container_id}" + " --format '{{.Image}}'").out
+            image_digest = X(f"docker image inspect {image_id}" + " --format '{{.RepoDigests}}'").out
+            remote_digest = X(f"regctl image digest {image}").out
+            is_up_to_date = remote_digest in image_digest
+
+        return ContainerDetails(image_name, service_name, compose_file_path, build, is_up_to_date)
     except ShellCommandError as e:
         Log.warn(f"An error occured while getting the details for container: {image} ({container_id})")
         Log.error(e.out)
@@ -65,20 +73,36 @@ def main() -> None:
     for compose_file_path, containers in compose_files_to_containers.items():
         Log.info(f"\n== {compose_file_path} ==")
         for container in containers:
-            status = "Up to date" if container.is_up_to_date else Color.success("Update found")
+            status = ""
+            if container.is_up_to_date:
+                status = "Up to date"
+            elif container.build:
+                Color.warn("Build")
+            else:
+                Color.success("Update found")
+
             print(f"{Color.warn(container.service_name)}[{container.image_name}]: {status}")
 
         for container in containers:
             if not container.is_up_to_date:
                 Log.warn(f"Updating {container.service_name}[{container.image_name}]...")
                 compose_file_dir = compose_file_path.parent
-                X(
-                    [
-                        f"cd {compose_file_dir}",
-                        f"docker pull {container.image_name}",
-                        f"docker compose -f {compose_file_path} up -d {container.service_name}",
-                    ]
-                )
+                if container.build:
+                    X(
+                        [
+                            f"cd {compose_file_dir}",
+                            f"docker compose -f {compose_file_path} build --no-cache --pull {container.service_name}",
+                            f"docker compose -f {compose_file_path} up -d {container.service_name}",
+                        ]
+                    )
+                else:
+                    X(
+                        [
+                            f"cd {compose_file_dir}",
+                            f"docker pull {container.image_name}",
+                            f"docker compose -f {compose_file_path} up -d {container.service_name}",
+                        ]
+                    )
 
 
 if __name__ == "__main__":
