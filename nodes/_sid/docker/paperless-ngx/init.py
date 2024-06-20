cc70cfe016282ef023fb9e26ac362d45b5e76f98
Adam Langbert <adamhl@pm.me>
2024-06-19 21:19:01 -0500

    fix: paperless init

--- nodes/_sid/docker/paperless-ngx/init.py
+++ nodes/_sid/docker/paperless-ngx/init.py
@@ -7,12 +7,12 @@ from shellrunner import X
 def main() -> None:
     generate_docker_env(["paperless_secret_key"], __file__)
     X("docker compose pull")
-    X("docker compose up -d db")
+    X("docker compose up -d postgres")
     time.sleep(15)
     X("docker compose stop")
     homelab_password = X("""sops -d --extract "['homelab_password']" ~/secrets.yaml""", show_output=False).out
     X(
-        f"docker compose run --rm -e DJANGO_SUPERUSER_PASSWORD='{homelab_password}' webserver createsuperuser --noinput --username 'adam' --email 'adamhl@pm.me'",  # noqa: E501
+        f"docker compose run --rm -e DJANGO_SUPERUSER_PASSWORD='{homelab_password}' paperless-ngx createsuperuser --noinput --username 'adam' --email 'adamhl@pm.me'",  # noqa: E501
     )
 
 
