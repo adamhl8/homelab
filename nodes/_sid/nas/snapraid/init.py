1769c286c11a2eaeb5ef904dcd6e0b534ec41a23
Adam Langbert <adamhl@pm.me>
2024-06-21 16:57:48 -0500

    fix: cleanup snapshots during snapraid init

--- nodes/_sid/nas/snapraid/init.py
+++ nodes/_sid/nas/snapraid/init.py
@@ -1,5 +1,5 @@
 from hl_helpers import homelab_paths as paths
-from shellrunner import X
+from shellrunner import ShellCommandError, X
 
 
 def main() -> None:
@@ -7,6 +7,18 @@ def main() -> None:
     X("mkdir -p ~/snapraid/")
     X("~/bin/snapraid_update.py")
     X(f"ln -f -s {paths.nodes.sid}/nas/snapraid/snapraid.conf ~/snapraid/")
+
+    # cleanup
+    try:
+        X("sudo btrfs subvolume delete /mnt/disk*/.snapshots/*/snapshot")
+    except ShellCommandError as e:
+        if "No matches for wildcard" not in e.out:
+            raise
+    try:
+        X("sudo rm -rf /mnt/disk*/.snapshots/")
+    except ShellCommandError as e:
+        if "No matches for wildcard" not in e.out:
+            raise
 
     # snapper
     X("sudo apt purge --auto-remove snapper -y")
