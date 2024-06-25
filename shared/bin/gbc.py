199b58e5ca0cf402ef529846e5f850f5b77d9fc7
Adam Langbert <adamhl@pm.me>
2024-06-25 16:39:20 -0500

    feat: git sanitize

--- shared/bin/gbc.py
+++ shared/bin/gbc.py
@@ -5,19 +5,12 @@ from shellrunner import X
 
 def main() -> None:
     X("git fetch -p", show_command=False)
-    branches = X("git branch -vv", show_command=False, show_output=False).out.splitlines()
-    gone_list = [line for line in branches if ": gone" in line]
+    output = X("git branch -vv", show_command=False, show_output=False).out.splitlines()
+    # If the currently checked out branch is gone, it will have a "*" prefix
+    branches = [line.strip().removeprefix("* ").split()[0] for line in output if ": gone" in line]
 
-    branch_list: list[str] = []
-    for branch in gone_list:
-        branch_split = branch.split()
-        if branch_split[0] == "*":
-            branch_list.append(branch_split[1])
-        else:
-            branch_list.append(branch_split[0])
-
-    for branch in branch_list:
-        X(f"git branch -D {branch}", show_command=False)
+    for line in branches:
+        X(f"git branch -D {line}", show_command=False)
 
 
 if __name__ == "__main__":
