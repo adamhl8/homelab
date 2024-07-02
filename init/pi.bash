08536ffd936f1fc678a5a157c4c5463d257e9f9c
Adam Langbert <adamhl@pm.me>
2024-07-01 19:32:25 -0500

    node: pi fish

--- init/pi.bash
+++ init/pi.bash
@@ -1,7 +1,5 @@
 shared="${shared:?}"
 
-# shellcheck source=./shared/homebrew.bash
-source "${shared}/homebrew.bash"
 # shellcheck source=./shared/rye.bash
 source "${shared}/rye.bash"
 # shellcheck source=./shared/shellrunner.bash
--- nodes/pi.py
+++ nodes/pi.py
@@ -1,12 +1,30 @@
+from hl_helpers import add_apt_source, start_all_docker_containers
 from hl_helpers import homelab_paths as paths
-from hl_helpers import start_all_docker_containers
-from shellrunner import X
+from shellrunner import ShellCommandError, X
 
 from shared._modules import shared
 
 
 def step1() -> None:
-    shared.fish_install()
+    X("mkdir -p ~/.config/fish/conf.d/")
+    X(f"ln -f -s {paths.configs.fish_config} ~/.config/fish/")
+
+    add_apt_source(
+        name="fish",
+        gpg_url="https://download.opensuse.org/repositories/shells:fish:release:3/Debian_12/Release.key",
+        source="http://download.opensuse.org/repositories/shells:/fish:/release:/3/Debian_12/ /",
+    )
+
+    X("sudo apt update && sudo apt install fish -y")
+    fish_path = X("which fish").out
+    try:
+        X("grep -q fish /etc/shells")
+    except ShellCommandError:
+        X(f"echo {fish_path} | sudo tee -a /etc/shells >/dev/null")
+        print(f"Added {fish_path} to /etc/shells")
+
+    X(f"chsh -s {fish_path}")
+    print(f"Set {fish_path} as default shell")
 
 
 def step2() -> None:
