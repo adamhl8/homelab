1976fcfd30ae40141aed2c85650453b8d8e7bf09
Adam Langbert <adamhl@pm.me>
2024-07-01 19:47:03 -0500

    pi: stuff

--- nodes/pi.py
+++ nodes/pi.py
@@ -1,4 +1,4 @@
-from hl_helpers import add_apt_source, start_all_docker_containers
+from hl_helpers import add_apt_source, get_latest_github_release, start_all_docker_containers
 from hl_helpers import homelab_paths as paths
 from shellrunner import ShellCommandError, X
 
@@ -45,8 +45,23 @@ def step2() -> None:
 
 
 def step3() -> None:
-    shared.sops()
-    shared.ssh()
+    sops_path = get_latest_github_release(
+        "getsops/sops",
+        r"sops.*linux\.arm64",
+        "~/bin/sops",
+    )
+    X(f"chmod 755 {sops_path}")
+
+    X(f"ln -f -s {paths.secrets_yaml} ~/")
+
+    X("mkdir -p ~/.config/sops/age/")
+    print("Enter key.age passphrase")
+    X(f"age -o ~/.config/sops/age/keys.txt -d {paths.age_key}")
+    X("chmod 600 ~/.config/sops/age/keys.txt")
+
+    X("mkdir -p ~/.ssh/")
+    X("chmod 700 ~/.ssh/")
+    X(f"ln -f -s {paths.configs.authorized_keys} ~/.ssh/")
     shared.sshd()
     shared.docker()
 
