d530bb87e1c337dcd99152c4f7203af7ed8f664f
Adam Langbert <adamhl@pm.me>
2024-11-07 13:26:36 -0600

    macbook apps

--- nodes/_macbook/apps.py
+++ nodes/_macbook/apps.py
@@ -56,14 +56,18 @@ def install_brew_casks(casks: list[str]) -> None:
 
 
 app_store_app_ids = [
+    "1099120373",  # ProtonMail Exporter
+    "1639917298",  # Onigiri
+    "1295203466",  # Windows App
+    "1545870783",  # System Color Picker
+    "414568915",  # Key Codes
+    "1558360383",  # Menu Bar Calendar
+    "490179405",  # Okta Verify
+    "1193539993",  # Brother iPrint Scan
     "470158793",  # Keka
     "1565701763",  # AudioWrangler
     "6446061552",  # Signal Shifter
     "1611378436",  # Pure Paste
-    "1558360383",  # Menu Bar Calendar
-    "1545870783",  # System Color Picker
-    "1639917298",  # Onigiri
-    "414568915",  # Key Codes
 ]
 
 
